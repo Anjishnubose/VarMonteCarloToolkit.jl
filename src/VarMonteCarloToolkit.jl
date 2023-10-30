a8ed21e7fdd1bdbf69437dbe73030010e94694df
Anjishnu Bose <anjishnubose98@gmail.com>
2023-10-30 19:29:08 -0400

    package module

--- src/VarMonteCarloToolkit.jl
+++ src/VarMonteCarloToolkit.jl
@@ -1,5 +1,12 @@
 module VarMonteCarloToolkit
 
 # Write your package code here.
+    include("MCConfig.jl")
+    using .MCConfig
+    export Config, FastUpdateConfig!, UpdateSlater!, GetSiteTuple, GetFockState, GetOccupancy
+
+    include("MCEstimator.jl")
+    using .MCEstimator
+    export LocalEstimator
 
 end
